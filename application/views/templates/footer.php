e8f1c380e0bf8b7eaddb829c6c6be852aa631dc6
Rio Yudha Baskara <156874101+RioYBaskara@users.noreply.github.com>
2024-07-05 11:07:53 +0700

    Menambahkan sweetalert2

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -5,7 +5,10 @@
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js"
     integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
     crossorigin="anonymous"></script>
+<script src="<?= base_url(); ?>assets/js/sweetalert2.all.min.js"></script>
 <script src="<?= base_url(); ?>assets/js/bootstrap.js"></script>
+</body>
+<script src="<?= base_url(); ?>assets/js/myscript.js"></script>
 </body>
 
 </html>
---
+++ assets/js/myscript.js
@@ -0,0 +1,6 @@
+Swal.fire({
+	title: "Tes SweetAlert2",
+	text: "Tes SweetAlert2",
+	type: "success",
+	icon: "success",
+});
