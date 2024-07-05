8b7174bcb81a7edfb4b5c1c5dbd7dbf1ab98e294
Rio Yudha Baskara <156874101+RioYBaskara@users.noreply.github.com>
2024-07-05 10:33:36 +0700

    Mengubah Default Controller ke Home.php

---
+++ application/views/templates/footer.php
@@ -0,0 +1,11 @@
+<!-- FOOTER TEMPLATE -->
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="<?= base_url(); ?>assets/js/jquery-3.7.1.min.js" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js"
+    integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+    crossorigin="anonymous"></script>
+<script src="<?= base_url(); ?>assets/js/bootstrap.js"></script>
+</body>
+
+</html>
